e40301f8e5de412719944632e8f788a81bd9b167
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-03-16 23:12:07 +0400

    Added Person model with cell phone validation

--- plays/models.py
+++ plays/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
+from django.db import models
+from django.core.exceptions import ValidationError
+import re
 # Create your models here.
+
+class Person(models.Model):
+    GOALKEEPER = 'G'
+    BACK = 'B'
+    HALFBACK = 'H'
+    FORWARD = 'F'
+    COACH = 'C'
+    POSITION_CHOICES = (
+        (GOALKEEPER, 'Вратарь'),
+        (BACK, 'Защитник'),
+        (HALFBACK, 'Полузащитник'),
+        (FORWARD, 'Нападающий'),
+        (COACH, 'Тренер'),
+    )
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=1, choices=POSITION_CHOICES)
+    is_active = models.BooleanField(default=True)
+    start_date = models.DateField(auto_now=False, )
+    finish_date = models.DateField(auto_now=False)
+    cell_phone = models.CharField(max_length=12)
+    email = models.EmailField()
+
+    def clean(self):
+        cell_phone_template = "(([+]7)|8)\d{10}"
+        if not (re.match(cell_phone_template, self.cell_phone)):
+            raise ValidationError("Invalid cell phone format: %s" % self.cell_phone)
