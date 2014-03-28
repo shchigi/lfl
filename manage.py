244f3b65264ff063379fb49609e9b04a5edfad94
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-03-28 23:49:14 +0600

    Added insert test data in manage.py

--- manage.py
+++ manage.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+    from plays.add_test_data import add_test_data
+    add_test_data()
